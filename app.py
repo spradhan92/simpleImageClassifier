910b4821f0f610e5e8be7c6127e83f291aaf7ec2
Sripada Pradhan <sripada.pradhan@protonmail.com>
2022-02-09 14:41:16 +0100

    Updated app.py

--- app.py
+++ app.py
@@ -79,6 +79,5 @@ def predict():
     return None
 
 
-# if __name__ == '__main__':
-# #    app.run(debug=False)
-#      app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8000)
